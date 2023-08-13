bffb6786195d68a3570949fc5e5f7c7b8bf1717d
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-13 12:35:04 -0300

    font awesome icon install

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,46 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const inter = Inter({ subsets: ['latin'] })
+import { faHouse, faMessages, faDollarSign, faBell, faUser } from '@fortawesome/pro-solid-svg-icons';
+import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {' '}
-            <ConnectButton
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
+      <a
+        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {' '}
+        <ConnectButton
+          className="dark:invert"
+          width={100}
+          height={24}
+          priority
+        />
+      </a>
+      <footer className="fixed bottom-0 left-0 flex w-full h-24 bg-white text-black items-center justify-around border-t-2 border-blue-500">
+        <a href="/home" className="flex flex-col items-center font-semibold">
+          <FontAwesomeIcon icon={faHouse} />
+          <span>Home</span>
+        </a>
+        <a href="/messages" className="flex flex-col items-center font-semibold">
+          <FontAwesomeIcon icon={faMessages} />
+          <span>Messages</span>
+        </a>
+        <a href="/pay-receive" className="flex flex-col items-center font-semibold">
+          <FontAwesomeIcon icon={faDollarSign} />
+          <span>Pay / Receive</span>
+        </a>
+        <a href="/notifications" className="flex flex-col items-center font-semibold">
+          <FontAwesomeIcon icon={faBell} />
+          <span>Notifications</span>
+        </a>
+        <a href="/profile" className="flex flex-col items-center font-semibold">
+          <FontAwesomeIcon icon={faUser} />
+          <span>Profile</span>
+        </a>
+      </footer>
     </main>
-  )
+  );
 }

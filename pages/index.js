dd26ca391ec37d27ed943fde12056c69d0becc42
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-13 20:15:55 -0300

    Favicon and SEO added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse, faMessages, faDollarSign, faBell, faUser, faMagnifyingGlass, faBarcodeRead } from '@fortawesome/pro-solid-svg-icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
@@ -8,10 +9,23 @@ export default function Messages() {
   const [activeTab, setActiveTab] = useState('messages');
 
   return (
-    <main className="flex flex-col min-h-screen p-0 bg-white">
+      <main className="flex flex-col min-h-screen p-0 bg-white">
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+        <link rel="canonical" href="https://www.basepay.app" />
+        <title>BasePay.app - Go cashless, Go Decentralized!</title>
+        <meta name="description" content="BasePay streamlines payments for all social situations, ensuring they're more convenient than before. Go cashless, Go Decentralized with BasePay.app."/>
+        <meta property="og:title" content="BasePay.app: Your Decentralized Payment Solution" />
+        <meta property="og:description" content="Whether you're organizing a vacation or sharing lunch expenses, BasePay streamlines payments for all social situations." />
+        <meta property="og:url" content="https://www.basepay.app" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="BasePay.app: Go cashless, Go Decentralized!"/>
+        <meta name="twitter:description" content="BasePay streamlines payments for all social situations, ensuring they're more convenient than before."/>
+        <meta name="keywords" content="Crypto, Decentralized, Payments, Wallet, Metamask, BasePay.app, Contacts, Transactions, Ether, Base"/>
+      </Head>
       <div className="px-4 pb-4 pt-6 flex items-center">
         <div className="flex items-center border-2 border-blue-500 rounded-3xl w-full p-2">
-          <FontAwesomeIcon icon={faMagnifyingGlass} className="pl-2 mr-2 text-black" />
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="pl-2 mr-2 text-black w-6 h-6" />
           <input
             type="text"
             placeholder="Search for an ENS or Base address..."

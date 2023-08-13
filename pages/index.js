0bfc02be58368b72ad20b2628363b42ad16b7718
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-13 16:31:41 -0300

    Footer staging

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,29 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faHouse, faMessages, faDollarSign, faBell, faUser } from '@fortawesome/pro-solid-svg-icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <a
-        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {' '}
-        <ConnectButton
-          className="dark:invert"
-          width={100}
-          height={24}
-          priority
-        />
-      </a>
-      <footer className="fixed bottom-0 left-0 flex w-full h-24 bg-white text-black items-center justify-around border-t-2 border-blue-500">
+    <main className="flex flex-col min-h-screen p-0 bg-white">
+      <div className="flex-grow flex items-center justify-center">
+        <a
+          className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {' '}
+          <ConnectButton
+            className="dark:invert"
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+      <footer className="relative flex w-full h-24 bg-white text-black items-center justify-around border-t-2 border-blue-500">
+        <div className="absolute top-[-30px] left-1/2 transform -translate-x-1/2 w-[60px] h-[60px] rounded-full bg-white border-b-2 border-blue-500 flex items-center justify-center">
+          <FontAwesomeIcon icon={faDollarSign} />
+        </div>
         <a href="/home" className="flex flex-col items-center font-semibold">
           <FontAwesomeIcon icon={faHouse} />
           <span>Home</span>
@@ -28,9 +32,8 @@ export default function Home() {
           <FontAwesomeIcon icon={faMessages} />
           <span>Messages</span>
         </a>
-        <a href="/pay-receive" className="flex flex-col items-center font-semibold">
-          <FontAwesomeIcon icon={faDollarSign} />
-          <span>Pay / Receive</span>
+        <a href="/pay-receive" className="flex flex-col items-center font-semibold invisible">
+          <span>Pay</span>
         </a>
         <a href="/notifications" className="flex flex-col items-center font-semibold">
           <FontAwesomeIcon icon={faBell} />

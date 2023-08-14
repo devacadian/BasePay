b4eedce022c98f8974a67e76977c18c88af19952
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-14 12:25:48 -0300

    pay stagging continued

--- pages/pay.js
+++ pages/pay.js
@@ -14,18 +14,21 @@ const Pay = () => {
   };
 
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center">
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center pb-24">
       <Head>
         <title>Payment Page</title>
         <meta name="description" content="Handle payments here" />
       </Head>
-      <main className="text-center">
-        <div className="text-4xl font-semibold mb-8 text-black">$ {counter || '0.00'}</div>
-        <div className="grid grid-cols-3 gap-4">
+      <main className="text-center mt-20">
+        <div className="text-4xl font-semibold mb-8 text-black flex justify-center items-baseline -ml-5">
+          <span>$</span>
+          <span className="text-center">{counter || '0'}</span>
+        </div>
+        <div className="grid grid-cols-3 gap-x-20 gap-y-10 mb-8 mt-10">
           {[1, 2, 3, 4, 5, 6, 7, 8, 9, '.', 0, '<'].map((number, index) => (
             <button
               key={index}
-              className="p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-300 text-black"
+              className="p-4 rounded-lg focus:outline-none focus:border-blue-300 text-black text-xl font-semibold"
               onClick={() => (number === '<' ? handleBackspace() : handleNumberClick(number))}
             >
               {number}
@@ -33,6 +36,10 @@ const Pay = () => {
           ))}
         </div>
       </main>
+      <div className="w-full flex justify-center space-x-3 px-4">
+        <button className="w-1/2 bg-base-blue text-black text-lg font-medium flex items-center justify-center h-12 rounded-3xl focus:outline-none">Pay</button>
+        <button className="w-1/2 bg-base-blue text-black text-lg font-medium flex items-center justify-center h-12 rounded-3xl focus:outline-none">Request</button>
+      </div>
     </div>
   );
 };

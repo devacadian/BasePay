759b96226d3a3e37de202bb04b45b217d87bdda0
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-14 17:19:21 -0300

    Base Goerli set as default chain

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css';
 import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { arbitrum, goerli, mainnet, optimism, polygon, zora } from 'wagmi/chains';
+import { arbitrum, goerli, mainnet, optimism, polygon, zora, baseGoerli } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import Head from 'next/head';
@@ -9,7 +9,7 @@ import Footer from '../components/Footer';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
-    mainnet, polygon, optimism,arbitrum,zora, ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
+    mainnet, polygon, optimism, baseGoerli, arbitrum, zora, ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
 ],
   [publicProvider()]
 );
@@ -42,7 +42,7 @@ function MyApp({ Component, pageProps }) {
         />
       </Head>
     <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider coolMode chains={chains} >
+      <RainbowKitProvider coolMode chains={chains} initialChain={baseGoerli} >
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>

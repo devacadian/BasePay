7d815aa1c41966f6bc31917151bca554c123d0e7
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-15 12:44:39 -0300

    gradient added to pay page

--- pages/pay.js
+++ pages/pay.js
@@ -55,36 +55,40 @@ const Pay = () => {
 
 
   return (
-    <div className="min-h-screen bg-white flex flex-col">
-      <Head>
-        <title>Payment Page</title>
-        <meta name="description" content="Handle payments here" />
-      </Head>
-      <div className="px-4 pb-0 pt-8 flex items-center w-full justify-between">
+    <div className="min-h-screen flex flex-col bg-white pb-20">
+      <div style={{ background: 'linear-gradient(to bottom, #007BFF, #ffffff)' }} className="flex-grow flex flex-col">
+        <Head>
+          <title>Payment Page</title>
+          <meta name="description" content="Handle payments here" />
+        </Head>
+        <div className="px-4 pb-0 pt-8 flex items-center w-full justify-between">
         <div className="flex items-center">
-        <FontAwesomeIcon icon={faClockNine} className="mr-4 h-7 w-7 text-gray-600" />
-        <div className={`${containerWidth} h-8 border rounded-4xl border-2.5 border-gray-600 flex items-center justify-center text-xs text-black font-semibold`}
-             onClick={openChainModal}>
-          {isClient && isBaseGoerli ? (
-            <>
-              <img src="/assets/Base_Network_Logo.svg" alt="Network Logo" className="mr-1 h-3 w-3" />
-              <FontAwesomeIcon icon={faEthereum} className="mr-1 text-black h-3 w-3" />
-            </>
-          ) : (
-            <FontAwesomeIcon icon={faEthereum} className="mr-1 text-black h-3 w-3" />
-          )}
-          {formattedBalance}
+  <div className="bg-blue-100 rounded-full h-7 w-7 flex items-center justify-center"> 
+    <FontAwesomeIcon icon={faClockNine} className="h-7 w-7 text-base-blue" />
+  </div>
+  <div className={`${containerWidth} h-8 rounded-4xl bg-blue-100 border-base-blue border-2 flex items-center justify-center text-xs text-black font-semibold ml-4`}
+       onClick={openChainModal}>
+    {isClient && isBaseGoerli ? (
+      <>
+        <img src="/assets/Base_Network_Logo.svg" alt="Network Logo" className="mr-1 h-3 w-3" />
+        <FontAwesomeIcon icon={faEthereum} className="mr-1 text-black h-3 w-3" />
+      </>
+    ) : (
+      <FontAwesomeIcon icon={faEthereum} className="mr-1 text-black h-3 w-3" />
+    )}
+    {formattedBalance}
+  </div>
 </div>
+<div className="flex items-center relative">
+          <div className=" bg-blue-100 absolute h-6 w-7 rounded-md"  />
+          <FontAwesomeIcon icon={faBarcodeRead} className="h-7 w-7 text-base-blue z-10" />
         </div>
-        <FontAwesomeIcon icon={faBarcodeRead} className="ml-4 mr-0 h-7 w-7 text-gray-600" />
       </div>
-      <div className="flex-grow flex flex-col items-center justify-center pb-24">
-        <main className="text-center mt-0">
-        <div className="text-6xl font-semibold mb-8 text-black flex justify-center items-baseline -ml-10">
-            <FontAwesomeIcon icon={faEthereum} className="mr-0 text-black h-10 w-10" /> {/* Ethereum icon */}
-            <span className="text-center">{counter || '0'}</span>
-          </div>
-          <div className="grid grid-cols-3 gap-x-20 gap-y-8 mb-8 mt-10">
+      <div className="flex-grow flex flex-col items-center justify-center mt-10 mb-10 relative">
+  <span className="text-center text-5xl font-semibold text-black">{counter || '0'}</span>
+  <FontAwesomeIcon icon={faEthereum} className="text-black h-8 w-8 absolute left-1/2 -ml-13" />
+</div>
+          <div className="grid grid-cols-3 gap-x-0 gap-y-8 mb-8 mt-0">
             {[1, 2, 3, 4, 5, 6, 7, 8, 9, '.', 0, '<'].map((number, index) => (
               <button
                 key={index}
@@ -95,17 +99,16 @@ const Pay = () => {
               </button>
             ))}
           </div>
-        </main>
-        <div className="w-full flex justify-center space-x-3 px-4">
+      </div>
+      <div className="w-full flex justify-center space-x-3 px-4 mb-20">
         <button onClick={handlePayClick} className="w-1/2 bg-base-blue text-white text-lg font-medium flex items-center justify-center h-12 rounded-3xl focus:outline-none">
           <FontAwesomeIcon icon={faPaperPlane} className="mr-2 h-4 w-4 text-white" />
           Pay
         </button>
         <button className="w-1/2 bg-base-blue text-white text-lg font-medium flex items-center justify-center h-12 rounded-3xl focus:outline-none">
-          <FontAwesomeIcon icon={faFileInvoice} className="mr-2 h-4 w-4 text-white" /> 
+          <FontAwesomeIcon icon={faFileInvoice} className="mr-2 h-4 w-4 text-white" />
           Request
         </button>
-      </div>
       </div>
     </div>
   );

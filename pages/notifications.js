198a33c9c27c6ecb9af3ad3fc419ad1c9f7f6ce6
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-15 09:31:41 -0300

    notifcations page staging

---
+++ pages/notifications.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Notifications = () => {
+  return (
+    <main className="flex flex-col min-h-screen p-0 bg-white">
+      <Head>
+        <link rel="canonical" href="https://www.basepay.app/notifications" />
+        <title>BasePay.app Notifications - Stay Updated!</title>
+        <meta name="description" content="Get the latest notifications and updates related to your BasePay activities. Stay informed with BasePay.app."/>
+        <meta property="og:title" content="BasePay.app Notifications: Your Decentralized Payment Updates" />
+        <meta property="og:description" content="Stay updated with all your BasePay notifications and activities." />
+        <meta property="og:url" content="https://www.basepay.app/notifications" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="BasePay.app Notifications: Stay Updated!"/>
+        <meta name="twitter:description" content="Get the latest notifications and updates related to your BasePay activities."/>
+        <meta name="keywords" content="Crypto, Decentralized, Payments, Wallet, Metamask, BasePay.app, Notifications, Updates, Ether, Base"/>
+      </Head>
+      {/* Content goes here */}
+    </main>
+  );
+};
+
+export default Notifications;

338f2d435ffa639dc74b4385168e1ad374ec6057
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-17 13:24:11 -0300

    Message Page continued

--- pages/messages.js
+++ pages/messages.js
@@ -21,19 +21,36 @@ export default function Messages() {
           <FontAwesomeIcon icon={faMagnifyingGlass} className="h-6 w-6 text-black align-middle mt-3" /> {/* Magnifying glass icon */}
         </button>
       </div>
+
       <div className="flex items-start p-4">
-  <div className="bg-base-blue w-14 h-14 rounded-full flex-shrink-0"></div> {/* Circle */}
+  {/* First Bubble */}
+  <div className="bg-gray-300 w-14 h-14 rounded-full flex-shrink-0"></div>
   <div className="ml-4 flex flex-col flex-grow">
     <div className="grid grid-cols-[auto,1fr,auto] items-center gap-x-2">
-      <p className="text-black text-xl font-semibold mt-1 truncate">Acadian.eth</p> {/* Name with truncation */}
-      <span className="text-black text-sm font-semibold justify-self-end">12h</span> {/* "12h" text */}
+      <p className="text-black text-xl font-semibold mt-1 truncate">Acadian.eth</p>
+      <span className="text-black text-sm font-semibold justify-self-end">12h</span>
     </div>
     <div className="grid grid-cols-[1fr,auto] items-center gap-x-2">
-      <p className="text-black text-sm truncate overflow-hidden whitespace-nowrap">Hey what are your plans this weekend? test test test this should truncate</p> {/* Text with truncation */}
-      <div className="bg-base-blue w-9 h-6 rounded-xl flex items-center justify-center text-white text-xs">1</div> {/* Circle with "1" */}
+      <p className="text-black text-sm truncate overflow-hidden whitespace-nowrap">Hey what are your plans this weekend? test test test this should truncate</p>
+      <div className="bg-base-blue w-9 h-6 rounded-xl flex items-center justify-center text-white text-xs">1</div>
     </div>
   </div>
 </div>
+
+<div className="flex items-start p-4 mt-0"> {/* Second Bubble with mt-4 */}
+  <div className="bg-gray-300 w-14 h-14 rounded-full flex-shrink-0"></div>
+  <div className="ml-4 flex flex-col flex-grow">
+    <div className="grid grid-cols-[auto,1fr,auto] items-center gap-x-2">
+      <p className="text-black text-xl font-semibold mt-1 truncate">0x7bF92...</p>
+      <span className="text-black text-sm font-semibold justify-self-end">12h</span>
+    </div>
+    <div className="grid grid-cols-[1fr,auto] items-center gap-x-2">
+      <p className="text-black text-sm truncate overflow-hidden whitespace-nowrap">I was thinking we go out to that new restaurant around the block this weekend!</p>
+      <div className="bg-base-blue w-9 h-6 rounded-xl flex items-center justify-center text-white text-xs">27</div>
+    </div>
+  </div>
+</div>
+
       <div className="bg-white w-full -mb-2"></div>
       <div className="flex-grow flex items-center justify-center">
         {/* You can add other content here */}

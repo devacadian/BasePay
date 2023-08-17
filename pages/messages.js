77fe5bad0dcd450a849b18c7df8dd935fed811ba
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-17 11:33:55 -0300

    Message Search Skeleton

--- pages/messages.js
+++ pages/messages.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Head from 'next/head';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMessagePen } from '@fortawesome/pro-solid-svg-icons';
+import { faMessagePen, faMagnifyingGlass } from '@fortawesome/pro-solid-svg-icons';
 
 export default function Messages() {
   return (
@@ -13,19 +13,25 @@ export default function Messages() {
         <div className="flex items-center">
           <h1 className="text-black text-3xl font-semibold pt-2 mr-1">Messages</h1>
         </div>
-        {/* Other header content can be added here */}
+        <button
+          onClick={() => {
+            // Handle the search action here
+          }}
+        >
+          <FontAwesomeIcon icon={faMagnifyingGlass} className="h-6 w-6 text-black align-middle mt-3" /> {/* Magnifying glass icon */}
+        </button>
       </div>
       <div className="bg-white w-full -mb-2"></div>
       <div className="flex-grow flex items-center justify-center">
         {/* You can add other content here */}
       </div>
       <button
-        className="bg-base-blue h-15 w-15 flex items-center justify-center rounded-full text-white fixed bottom-40 right-4" // Changed from "absolute" to "fixed"
+        className="bg-base-blue h-12 w-12 flex items-center justify-center rounded-full text-white fixed bottom-36 right-4" // Changed from "absolute" to "fixed"
         onClick={() => {
           // Handle the create message action here
         }}
       >
-        <FontAwesomeIcon icon={faMessagePen} className="h-7 w-7" /> {/* Adjusted size */}
+        <FontAwesomeIcon icon={faMessagePen} className="h-6 w-6" /> {/* Icon size unchanged */}
       </button>
       <div className="h-24 bg-white w-full absolute bottom-0">
         {/* Footer content */}

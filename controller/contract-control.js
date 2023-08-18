a596f59a6bd3ef01c714ed4123a5418dc52e1e88
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-18 17:06:05 -0300

    more debugging, ignore

--- controller/contract-control.js
+++ controller/contract-control.js
@@ -24,42 +24,33 @@ async function connectNode(eth) {
 async function initiatePayment(eth, _paymentRecipient, _etherValue, callback) {
     try {
         const basePayContractInstance = await connectNode(eth);
-        const txResponse = await basePayContractInstance.initiatePayment(_paymentRecipient, {
-            value: ethers.utils.parseEther(_etherValue),
-            // gasLimit: 29000 // Setting a low gas limit to intentionally fail the transaction
-        });
-  
+        const txResponse = await basePayContractInstance.initiatePayment(_paymentRecipient, {value: ethers.utils.parseEther(_etherValue)});
         console.log("Transaction Hash: ", txResponse.hash);
-  
+
         // Return the transaction hash immediately
         const txHash = txResponse.hash;
-  
+
         // Wait for the transaction to be mined
         txResponse.wait().then(receipt => {
-            if (receipt) {
-                if (receipt.status === 1) {
-                    console.log("Transaction successful!");
-                } else {
-                    console.log("Transaction failed or reverted.");
-                }
-                // Call the callback with the receipt if provided
-                if (callback) callback(receipt);
+            if (receipt.status === 1) {
+                console.log("Transaction successful!");
             } else {
-                console.log("Receipt is null or undefined.");
-                if (callback) callback({status: 0}); // Call the callback with a failure status
+                console.log("Transaction failed or reverted.");
             }
-        }).catch(error => {
-            // Handle the error here
-            console.log("Transaction failed with an exception:", error.message);
-            if (callback) callback({status: 0}); // Call the callback with a failure status
+            // Call the callback with the receipt if provided
+            if (callback) callback(receipt);
         });
-  
+
         return txHash;
     } catch(error) {
         console.log(error.message);
         return null;
     }
-  }
+}
+
+module.exports = {
+    initiatePayment
+};
 
 module.exports = {
     initiatePayment

b60008a2910b067291eb4b2379dcbc63f1dbf3b7
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-18 23:16:49 -0300

    Home Page Staging 1

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,29 @@
-import React, { useState } from 'react';
-import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
-import { useRouter } from 'next/router'; // Import useRouter
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass, faBarcodeRead } from '@fortawesome/pro-solid-svg-icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { useBalance } from 'wagmi';
+import { useAccount } from "wagmi";
+import { faEthereum } from '@fortawesome/free-brands-svg-icons';
+
 
 export default function Home() {
+  const { address } = useAccount();
+  const { data } = useBalance({ address });
+  const [formattedBalance, setFormattedBalance] = useState('0.0000');
+
+  useEffect(() => {
+    const balanceValue = parseFloat(data?.formatted || '0.0000');
+    setFormattedBalance(balanceValue.toFixed(4));
+  }, [data]);
 
   return (
-      <main className="flex flex-col min-h-screen p-0 bg-white">
+    <main className="flex flex-col min-h-screen bg-white">
       <Head>
-        <link rel="canonical" href="https://www.basepay.app" />
-        <title>BasePay.app - Go cashless, Go Decentralized!</title>
-        <meta name="description" content="BasePay streamlines payments for all social situations, ensuring they're more convenient than before. Go cashless, Go Decentralized with BasePay.app."/>
-        <meta property="og:title" content="BasePay.app: Your Decentralized Payment Solution" />
-        <meta property="og:description" content="Whether you're organizing a vacation or sharing lunch expenses, BasePay streamlines payments for all social situations." />
-        <meta property="og:url" content="https://www.basepay.app" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="BasePay.app: Go cashless, Go Decentralized!"/>
-        <meta name="twitter:description" content="BasePay streamlines payments for all social situations, ensuring they're more convenient than before."/>
-        <meta name="keywords" content="Crypto, Decentralized, Payments, Wallet, Metamask, BasePay.app, Contacts, Transactions, Ether, Base"/>
+        {/* Head content */}
       </Head>
-      <div className="px-4 pb-4 pt-6 flex items-center">
+      <div className="pb-8 pt-6 px-4 flex items-center">
         <div className="flex items-center border-2 border-blue-500 rounded-3xl w-full p-2">
           <FontAwesomeIcon icon={faMagnifyingGlass} className="pl-2 mr-2 text-black w-6 h-6" />
           <input
@@ -33,6 +34,20 @@ export default function Home() {
         </div>
         <FontAwesomeIcon icon={faBarcodeRead} className="ml-4 mr-0 h-8 w-8 text-gray-600" />
       </div>
+      <div className="px-4"> {/* Container for padding */}
+        <div className="bg-gray-100 h-20 rounded-3xl flex items-center justify-start w-full shadow-sm drop-shadow-sm p-4"> {/* Wrapper div */}
+          <div className="flex justify-center items-center relative w-10 h-10 rounded-full bg-gray-300 shadow drop-shadow ml-4"> {/* Ethereum logo */}
+            <FontAwesomeIcon icon={faEthereum} className="text-black h-6 w-6 z-10" />
+          </div>
+          <div className="text-left text-black ml-4 flex flex-col">
+            <div>{formattedBalance} ETH</div> {/* Display formatted balance */}
+            <div className="text-sm text-black">Goerli Basechain Balance</div> {/* Label */}
+          </div>
+        </div>
+      </div>
+      <div className="px-4 mt-6 text-xl font-semibold text-black"> {/* Quick Actions */}
+        Quick Actions
+      </div>
       <div className="flex-grow flex items-center justify-center">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 pointer-events-auto lg:p-0"
@@ -40,14 +55,9 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           {' '}
-          <ConnectButton
-            className="dark:invert"
-            width={100}
-            height={24}
-            priority
-          />
+          <ConnectButton className="dark:invert" width={100} height={24} priority />
         </a>
       </div>
     </main>
-  );
+  );  
 }

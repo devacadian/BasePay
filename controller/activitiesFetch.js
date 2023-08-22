08ef5d7a072a168c301ac06098b9176d0c34ce0a
allentsangdev <allentsang04@gmail.com>
2023-08-22 12:31:57 -0400

    add activity fetch control

--- controller/activitiesFetch.js
+++ controller/activitiesFetch.js
@@ -1,5 +1,11 @@
-// first fetch all Request Type Activity from Firestore
+import axios from 'axios'
 
+// first fetch all Request Type Activity from Firestore
+async function fetchActivities(userAddress) {
+    const expressApiUrl = `https://basepay-api.onrender.com/activties/${userAddress}`
+    const response = await axios.get(expressApiUrl)
+    return activities.data
+}
 
 // fetch all Payment Type Activity from Etherscan
 

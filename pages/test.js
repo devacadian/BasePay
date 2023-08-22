d36797738c80ed56ddcb09ace8268a347ebe02c1
devacadian <135052510+devacadian@users.noreply.github.com>
2023-08-22 13:40:30 -0300

    test page setup

---
+++ pages/test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-white px-4">
+      <button className="bg-base-blue text-white font-medium rounded-full w-full py-2 mx-1 mb-4 flex items-center justify-center">
+        Test 1
+      </button>
+      <button className="bg-base-blue text-white font-medium rounded-full w-full py-2 mx-1 mb-4 flex items-center justify-center">
+        Test 2
+      </button>
+      <button className="bg-base-blue text-white font-medium rounded-full w-full py-2 mx-1 mb-4 flex items-center justify-center">
+        Test 3
+      </button>
+      <button className="bg-base-blue text-white font-medium rounded-full w-full py-2 mx-1 flex items-center justify-center">
+        Test 4
+      </button>
+    </div>
+  );
+};
+
+export default Test;
